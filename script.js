c6db0249a4181b362dad970dc0c276759ce8330b
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2022-10-19 17:54:56 -0500

    second team functionality added, buttons event improved

--- script.js
+++ script.js
@@ -5,43 +5,69 @@
 
 //TODO Repartir funcionalidad para cada equipo
 const teamOne = document.querySelector('.first').childNodes
-const playerUp = document.querySelectorAll('.firstTeam')
+const playerFirst = document.querySelectorAll('.firstTeam')
+const playerSecond = document.querySelectorAll('.secondTeam')
 const butttonOne = document.querySelector('.btn.one')
+const butttonTwo = document.querySelector('.btn.two')
+const scoreButton = document.querySelectorAll('.score')
 
 console.log(butttonOne)
-console.log(playerUp[0].getAttribute('position'))
-console.log(playerUp[0].innerText)
-console.log(playerUp)
+console.log(playerFirst[0].getAttribute('position'))
+console.log(playerFirst[0].innerText)
+console.log(playerFirst)
 
 
 // EVENT LISTENERS
 
-butttonOne.addEventListener('click', rotation)
+scoreButton.forEach((button)=>addEventListener('click', rotation))
 
 
 // FUNCTIONS
 
 // * Debe añadirse la clase y quitarse cuando acabe la animación con promesa
 
-function rotation(){
-    playerUp.forEach((player) => {
-        console.log(player.getAttribute('class') + " " + player.getAttribute('move'))
-        player.classList.add(player.getAttribute('move'))
-    })
-    setTimeout(changeNumber, 1999)
+function rotation(e){
+    if (e.target.value == 'one'){
+        playerFirst.forEach((player) => {
+            console.log(player.getAttribute('class') + " " + player.getAttribute('move'))
+            player.classList.add(player.getAttribute('move'))
+        })
+        setTimeout(changeNumberOne, 1999)
+    }
+    else if (e.target.value == 'two'){
+        playerSecond.forEach((player) => {
+            console.log(player.getAttribute('class') + " " + player.getAttribute('move'))
+            player.classList.add(player.getAttribute('move'))
+        })
+        setTimeout(changeNumberTwo, 1999)
+    }
 }
 
-console.log(playerUp[5].innerText, playerUp[4].innerText)
 
-function changeNumber(){
-    let space = playerUp[0].innerText
-    playerUp[0].innerText = playerUp[1].innerText
-    playerUp[1].innerText = playerUp[2].innerText
-    playerUp[2].innerText = playerUp[5].innerText
-    playerUp[5].innerText = playerUp[4].innerText
-    playerUp[4].innerText = playerUp[3].innerText
-    playerUp[3].innerText = space
-    playerUp.forEach((player) => {
+function changeNumberOne(){
+    let space = playerFirst[0].innerText
+    playerFirst[0].innerText = playerFirst[1].innerText
+    playerFirst[1].innerText = playerFirst[2].innerText
+    playerFirst[2].innerText = playerFirst[5].innerText
+    playerFirst[5].innerText = playerFirst[4].innerText
+    playerFirst[4].innerText = playerFirst[3].innerText
+    playerFirst[3].innerText = space
+    playerFirst.forEach((player) => {
+        player.classList.remove(player.getAttribute('move'))
+    })
+}
+
+console.log(playerSecond)
+
+function changeNumberTwo(){
+    let space = playerSecond[0].innerText
+    playerSecond[0].innerText = playerSecond[3].innerText
+    playerSecond[3].innerText = playerSecond[4].innerText
+    playerSecond[4].innerText = playerSecond[5].innerText
+    playerSecond[5].innerText = playerSecond[2].innerText
+    playerSecond[2].innerText = playerSecond[1].innerText
+    playerSecond[1].innerText = space
+    playerSecond.forEach((player) => {
         player.classList.remove(player.getAttribute('move'))
     })
 }
