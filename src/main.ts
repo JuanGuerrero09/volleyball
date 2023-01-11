d78bd04f69f9d0888c6b4097ef7c1621b3c63700
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-01-11 18:18:13 -0500

    vite project started

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,27 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import { setupCounter } from './counter'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
 // VARIABLES
 
 
@@ -25,6 +49,7 @@ scoreButton.forEach((button)=>addEventListener('click', rotation))
 // FUNCTIONS
 
 // * Debe añadirse la clase y quitarse cuando acabe la animación con promesa
+//TODO Añadir contador de puntos, iniciar poner clase de .serve y si está ahí no entra al loop
 
 function rotation(e){
     if (e.target.value == 'one'){
